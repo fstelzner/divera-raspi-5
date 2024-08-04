9975bcf2f300ca73edb2e7617f117f217fae82bc
fstelzner <florian.stelzner@wngmn.de>
2024-08-04 18:44:20 +0200

    Update start-chromium.sh

--- start-chromium.sh
+++ start-chromium.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chromium-browser --start-maximized --kiosk --incognito --user-data-dir=/home/pimon2/.config/chromium2 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter --app=https://app.divera247.com/monitor/1.html?autologin=[DEINE-DIVERA-AUTOLOGIN-ID] &
+chromium-browser --start-maximized --kiosk --incognito --user-data-dir=/home/pi/.config/chromium2 --enable-features=OverlayScrollbar,OverlayScrollbarFlashAfterAnyScrollUpdate,OverlayScrollbarFlashWhenMouseEnter --app=https://app.divera247.com/monitor/1.html?autologin=[DEINE-DIVERA-AUTOLOGIN-ID] &
